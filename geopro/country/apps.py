38e9276b9341bd50f2acb5097b106f8198082889
Vinyas <vinyasmusic@gmail.com>
2022-06-10 12:55:59 +0530

    Format with Black and add .idea to ignore

--- geopro/country/apps.py
+++ geopro/country/apps.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CountryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = "django.db.models.BigAutoField"
     name = "geopro.country"
     verbose_name = _("Country")
 
--- geopro/users/urls.py
+++ geopro/users/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-from geopro.users.views import (
-    user_detail_view,
-    user_redirect_view,
-    user_update_view,
-)
+from geopro.users.views import user_detail_view, user_redirect_view, user_update_view
 
 app_name = "users"
 urlpatterns = [
